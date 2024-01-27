1199ddcda8efe993fa683126b08caf86fd21f81e
jereycui2008 <jeremycuid@gmail.com>
2024-01-26 16:15:38 -0800

    Commit

--- _03_while_loops/_c_vending_machine.py
+++ _03_while_loops/_c_vending_machine.py
@@ -3,9 +3,11 @@ Calculate the value of the change (coins) the user has.
 """
 from tkinter import messagebox, simpledialog, Tk
 
+qwe = 0
 
 # Write you code under the if __name__ == '__main__': below
 def vending_machine(money):
+    global qwe
     items_for_sale = {"water" : 0.50, "soda" : 1.00, "pretzels" : 1.00, "candy bar" : 1.50, "exit" : 0.00}
 
     while True:
@@ -26,6 +28,14 @@ def vending_machine(money):
         else:
             messagebox.showwarning('Invalid item',
                                    'Please enter one of the following ' + str([item for item in items_for_sale]))
+            qwe = qwe + 1
+            if qwe == 5:
+                messagebox.showwarning(title=None, message="You just canceled 5 times.")
+            if qwe == 10:
+                messagebox.showwarning(title=None, message="Stop. You just canceled 10 times.")
+            if qwe == 15:
+                messagebox.showwarning(title=None, message="Okay. Get out. You just canceled 15 times.")
+                break
 
 # ======================= DO NOT EDIT THE CODE ABOVE =========================
 
@@ -37,19 +47,25 @@ if __name__ == '__main__':
     money_in_dollars = 3.00
 
     # TODO) Write a while loop that ends when you have no money left
-
+    while money_in_dollars > 0:
         # TODO) Call the vending_machine() function and save the money spent
         #  in a variable, for example:
         #  money_spent = vending_machine(money_in_dollars)
-
+        money_spent = vending_machine(money_in_dollars)
         # TODO) If no money was spent, tell the user how much money they still
         #  have and exit the while loop
-
+        if money_spent == 0:
+            messagebox.showinfo(title=None, message="You have $3.00 left.")
+            break
         # TODO) Otherwise, subtract the money spent from the amount of money
         #  you still have (money_in_dollars)
-
+        if money_spent > 0:
+            money_in_dollars = money_in_dollars - money_spent
     # TODO) If there is exactly 0 money left, create a message that
     #  congratulates the user because they maximized their money.
-
+        if money_in_dollars == 0.00:
+            messagebox.showinfo(title=None, message="You maximized your money.")
     # TODO) If there is a negative amount of money, tell the user they
     #  overspent!
+        if money_in_dollars < 0.00:
+            messagebox.showwarning(title=None, message="You overspent your money.")
